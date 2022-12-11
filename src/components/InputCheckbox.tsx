963388e6400fbc2fbf9fa201921e5b68ef4fe92b
Tom Tillistrand <tomtillistrand@gmail.com>
2022-12-11 18:28:34 -0500

    Fix checkbox height bug on Safari

--- src/components/InputCheckbox.tsx
+++ src/components/InputCheckbox.tsx
@@ -14,14 +14,14 @@ export default function InputCheckbox({
 }) {
   return (
     <label className="grid grid-cols-[1em_auto] items-center gap-6">
-      <div className="grid aspect-square w-full cursor-pointer place-items-center">
+      <div className="grid h-[1em] w-[1em] cursor-pointer place-items-center">
         <input
           type="checkbox"
           id={id}
           checked={value}
           name={id}
           onChange={handleChange}
-          className="col-start-1 row-start-1 h-full w-full cursor-pointer appearance-none border-2 border-almost-white checked:border-neon-green checked:bg-neon-green"
+          className="col-start-1 row-start-1 h-[1em] w-[1em] cursor-pointer appearance-none border-2 border-almost-white checked:border-neon-green checked:bg-neon-green"
         />
         <IconCheck className="col-start-1 row-start-1 h-auto w-2/3 text-dark-grey" />
       </div>

2b4c365c318ea5151b3f46ac2e0a532d3f71671f
Tom Tillistrand <tomtillistrand@gmail.com>
2022-12-11 17:57:42 -0500

    Add eslint ignore comment for explicit any exception

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -118,6 +118,7 @@ const getSelectedCharTypes = (options: Options): CharTypes[] => {
   return checkedOptions;
 };
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
 function isCheckboxOption(
   maybeOption: any
 ): maybeOption is keyof CheckboxOptions {
@@ -131,6 +132,7 @@ function isCheckboxOption(
   }
   return false;
 }
+/* eslint-enable */
 
 function App() {
   const [password, setPassword] = useState('');
